e2ea0dfe3c289cb5178d7bc3074fd894db69e8bf
Jeremy Chang <37570145+st88018@users.noreply.github.com>
2021-07-14 22:49:34 +0800

    0714

--- src/test.cpp
+++ src/test.cpp
@@ -31,7 +31,7 @@
 #include <mavros_msgs/State.h>
 #include "utils/kinetic_math.h"
 #include "utils/mission.h"
-// #include "utils/trajectories.h"
+#include "utils/trajectories.h"
 
 using namespace std;
 using namespace cv;
@@ -70,12 +70,6 @@ bool   UAV_flying = false;
 bool   stage_finished = false;
 bool   FSMinit = false;
 bool   pubtwist = false;
-/* Traj */
-deque<Vec8> trajectory1;
-Vec2 traj1_information;
-double Trajectory_timestep = 0.02;
-deque<Vec4> trajectory2; // For Twist test (time vx vy vz)
-Vec2 traj2_information;
 /* System */
 bool ROS_init = true;
 double System_initT,callback_LastT,System_LastT;
@@ -85,62 +79,6 @@ int LowSpeedcounter;
 mavros_msgs::State current_state;
 int coutcounter = 0;
 
-// void constantVtraj( Vec7 StartPose, Vec7 EndPose,double velocity,double angular_velocity){
-//   Quaterniond localq(StartPose[3],StartPose[4],StartPose[5],StartPose[6]);
-//   Vec3 localrpy = Q2rpy(localq);
-//   Vec3 start_rpy = Vec3(0,0,localrpy[2]);
-//   Vec3 start_xyz(StartPose[0],StartPose[1],StartPose[2]);
-//   Quaterniond endq(EndPose[3],EndPose[4],EndPose[5],EndPose[6]);
-//   Vec3 des_rpy = Q2rpy(endq);
-//   double dist = sqrt(pow((EndPose[0]-start_xyz[0]),2)+pow((EndPose[1]-start_xyz[1]),2)+pow((EndPose[2]-start_xyz[2]),2));
-//   double dist_duration = dist/velocity; // In seconds
-//   double duration; //total duration in seconds
-//   Vec3 vxyz = Vec3(((EndPose[0]-start_xyz[0])/dist)*velocity,((EndPose[1]-start_xyz[1])/dist)*velocity,((EndPose[2]-start_xyz[2])/dist)*velocity);
-//   if (start_rpy[2]>=M_PI)  start_rpy[2]-=2*M_PI;
-//   if (start_rpy[2]<=-M_PI) start_rpy[2]+=2*M_PI;
-//   if (des_rpy[2]>=M_PI)    des_rpy[2]-=2*M_PI;
-//   if (des_rpy[2]<=-M_PI)   des_rpy[2]+=2*M_PI;
-//   double d_yaw = des_rpy[2] - start_rpy[2];
-//   if (d_yaw>=M_PI)  d_yaw-=2*M_PI;
-//   if (d_yaw<=-M_PI) d_yaw+=2*M_PI;
-//   double yaw_duration = sqrt(pow(d_yaw/angular_velocity,2));
-//   if(yaw_duration>=dist_duration){duration = yaw_duration;}else{duration = dist_duration;}
-//   //initialize trajectory
-//   trajectory1.clear();
-//   double traj1_init_time = ros::Time::now().toSec();
-//   int wpc = duration/Trajectory_timestep;
-//   for(int i=0; i<wpc; i++){
-//       double dt = Trajectory_timestep*i;
-//       Vec3 xyz;
-//       Quaterniond q;
-//       // RPY
-//       if(dt<=yaw_duration){
-//           q = rpy2Q(Vec3(0,0,start_rpy[2]+dt*angular_velocity));
-//       }else{
-//           q = rpy2Q(des_rpy);
-//       }
-//       // Position_xyz
-//       if(dt<=duration){
-//           xyz = Vec3(start_xyz[0]+dt*vxyz[0],start_xyz[1]+dt*vxyz[1],start_xyz[2]+dt*vxyz[2]);
-//       }else{
-//           xyz << EndPose[0],EndPose[1],EndPose[2];
-//       }
-//       Vec8 traj1;
-//       traj1 << dt+traj1_init_time, xyz[0], xyz[1], xyz[2], q.w(), q.x(), q.y(), q.z();
-//       trajectory1.push_back(traj1);
-//   }
-// }
-// void gen_twist_traj(Vec3 vxyz, double duration){
-//     trajectory2.clear();
-//     double traj2_init_time = ros::Time::now().toSec();
-//     int wpc = duration/Trajectory_timestep;
-//     for(int i=0; i<wpc; i++){
-//         double dt = Trajectory_timestep*i;
-//         Vec4 traj2;
-//         traj2 << dt+traj2_init_time, vxyz[0], vxyz[1], vxyz[2];
-//         trajectory2.push_back(traj2);
-//     }
-// }
 void twist_pub(Vec3 vxvyvz){
     UAV_twist_pub.linear.x = vxvyvz(0);
     UAV_twist_pub.linear.y = vxvyvz(1);
@@ -168,11 +106,6 @@ void UAV_pub(bool pubtwist){
         uavposepub << traj1_deque_front[1],traj1_deque_front[2],traj1_deque_front[3],
                         traj1_deque_front[4],traj1_deque_front[5],traj1_deque_front[6],traj1_deque_front[7];
         pose_pub(uavposepub);
-
-        // (Trajectory current time > duration) && (current pos - target < 0.1m) than goes on to next stage
-        double dist2target = sqrt(( pow(traj1_deque_front[1]-UAV_lp[0],2))
-                                    +(pow(traj1_deque_front[2]-UAV_lp[1],2))
-                                    +(pow(traj1_deque_front[3]-UAV_lp[2],2)));
         if (traj1_deque_front[0] > traj1_information[1]){
             Mission_stage++;
             trajectory1.clear();
@@ -180,9 +113,6 @@ void UAV_pub(bool pubtwist){
             Zero7 << 0,0,0,0,0,0,0;
             pose_pub(Zero7);
         }
-        // else if((traj1_deque_front[0] - traj1_information[1]) > 1){
-        //     Mission_stage++;
-        // }
     }else{
         double current_time = ros::Time::now().toSec();
         Vec4 traj2_deque_front = trajectory2.front();
--- src/utils/trajectories.h
+++ src/utils/trajectories.h
@@ -2,6 +2,12 @@
 #define TRAJECTORIES_H
 #include "common.h"
 
+deque<Vec8> trajectory1;
+Vec2 traj1_information;
+double Trajectory_timestep = 0.02;
+deque<Vec4> trajectory2; // For Twist test (time vx vy vz)
+Vec2 traj2_information;
+
 void constantVtraj( Vec7 StartPose, Vec7 EndPose,double velocity,double angular_velocity){
   Quaterniond localq(StartPose[3],StartPose[4],StartPose[5],StartPose[6]);
   Vec3 localrpy = Q2rpy(localq);
