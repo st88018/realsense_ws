01891cd25f39f59801943f63bc20899e395a4c69
Jeremy Chang <37570145+st88018@users.noreply.github.com>
2021-09-20 14:04:21 +0800

    Update Failsafe.cpp

--- src/Failsafe.cpp
+++ src/Failsafe.cpp
@@ -12,11 +12,38 @@
 static mavros_msgs::State current_state;
 static geometry_msgs::PoseStamped UAV_pose_vicon,UGV_pose_vicon;
 static geometry_msgs::Twist       UAV_twist_pub;
+static Vec7 UAV_lp,UGV_lp;
 
+void ugv_pose_sub(const geometry_msgs::PoseStamped::ConstPtr& pose){
+    UGV_pose_sub.pose.position.x = pose->pose.position.x;
+    UGV_pose_sub.pose.position.y = pose->pose.position.y;
+    UGV_pose_sub.pose.position.z = pose->pose.position.z;
+    UGV_pose_sub.pose.orientation.w = pose->pose.orientation.w;
+    UGV_pose_sub.pose.orientation.x = pose->pose.orientation.x;
+    UGV_pose_sub.pose.orientation.y = pose->pose.orientation.y;
+    UGV_pose_sub.pose.orientation.z = pose->pose.orientation.z;
+    UGV_lp << UGV_pose_sub.pose.position.x,UGV_pose_sub.pose.position.y,UGV_pose_sub.pose.position.z,
+              UGV_pose_sub.pose.orientation.w,UGV_pose_sub.pose.orientation.x,UGV_pose_sub.pose.orientation.y,UGV_pose_sub.pose.orientation.z;
+}
+void uav_pose_sub(const geometry_msgs::PoseStamped::ConstPtr& pose){
+    UAV_pose_sub.pose.position.x = pose->pose.position.x;
+    UAV_pose_sub.pose.position.y = pose->pose.position.y;
+    UAV_pose_sub.pose.position.z = pose->pose.position.z;
+    UAV_pose_sub.pose.orientation.w = pose->pose.orientation.w;
+    UAV_pose_sub.pose.orientation.x = pose->pose.orientation.x;
+    UAV_pose_sub.pose.orientation.y = pose->pose.orientation.y;
+    UAV_pose_sub.pose.orientation.z = pose->pose.orientation.z;
+    UAV_lp << UAV_pose_sub.pose.position.x,UAV_pose_sub.pose.position.y,UAV_pose_sub.pose.position.z,
+              UAV_pose_sub.pose.orientation.w,UAV_pose_sub.pose.orientation.x,UAV_pose_sub.pose.orientation.y,UAV_pose_sub.pose.orientation.z;
+}
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "Failsafe");
     ros::NodeHandle nh;
+    ros::Subscriber ugvpose_sub = nh.subscribe<geometry_msgs::PoseStamped>
+                    ("/vrpn_client_node/gh034_car/pose", 5, ugv_pose_sub);
+    ros::Subscriber uavpose_sub = nh.subscribe<geometry_msgs::PoseStamped>
+                    ("mavros/local_position/pose", 1, uav_pose_sub);
     ros::Rate loop_rate(50); /* ROS system Hz */
 
     while(ros::ok()){
