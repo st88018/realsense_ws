9c8c2da748346464154f227900ce9116f9ddef0a
Jeremy Chang <37570145+st88018@users.noreply.github.com>
2021-10-21 13:57:15 +0800

    update .m s

--- logs/CSVtoMAT.m
+++ logs/CSVtoMAT.m
@@ -10,31 +10,29 @@ figure('Name','Position Estimation','Position',[0 0 850 850]);
 tiledlayout(3,1);
 
 nexttile
-plot(yolo(:,1),yolo(:,2),'Color','k','LineWidth',1);
-title('Position Estimation','FontSize',14);
-ylabel('X Position (m)','FontSize',14);
-hold on
+plot(yolo(:,1),yolo(:,2),'Color','k','LineWidth',1);hold on
+title('Position Estimation','FontSize',15);
+ylabel('X Position (m)','FontSize',15);
 plot(groundtruth(:,1),groundtruth(:,2),'Color','r','LineWidth',1);
 plot(kf(:,1),kf(:,2),'Color',[0 0.4470 0.7410],'LineWidth',1);
 plot(aruco(:,1),aruco(:,2),'Color','g','LineWidth',1);
 set(gca,'FontSize',14);
+legend('Yolo','GroundTruth','KF','Aruco','FontSize',15);
 
 nexttile
-plot(yolo(:,1),yolo(:,3),'Color','k','LineWidth',1);
-ylabel('Y Position (m)','FontSize',14);
-hold on
+plot(yolo(:,1),yolo(:,3),'Color','k','LineWidth',1);hold on
+ylabel('Y Position (m)','FontSize',15);
 plot(groundtruth(:,1),groundtruth(:,3),'Color','r','LineWidth',1);
 plot(kf(:,1),kf(:,3),'Color',[0 0.4470 0.7410],'LineWidth',1);
 plot(aruco(:,1),aruco(:,3),'Color','g','LineWidth',1);
 set(gca,'FontSize',14);
+legend('Yolo','GroundTruth','KF','Aruco','FontSize',15);
 
 nexttile
-plot(yolo(:,1),yolo(:,4),'Color','k','LineWidth',1);
-ylabel('Z Position (m)','FontSize',14);
-hold on
+plot(yolo(:,1),yolo(:,4),'Color','k','LineWidth',1);hold on
+ylabel('Z Position (m)','FontSize',15);
 plot(groundtruth(:,1),groundtruth(:,4),'Color','r','LineWidth',1);
 plot(kf(:,1),kf(:,4),'Color',[0 0.4470 0.7410],'LineWidth',1);
 plot(aruco(:,1),aruco(:,4),'Color','g','LineWidth',1);
-set(gca,'FontSize',14);
-
-legend('Yolo','GT','KF','Aruco','FontSize',10);
+set(gca,'FontSize',15);
+legend('Yolo','GroundTruth','KF','Aruco','FontSize',15);
